6f47659d1ca11e9745d8aa4d0e42c354c279813d
Hydrogeniouss <marco@volderauer.at>
2022-11-30 19:02:34 +0100

    Hacknet.js now includes hashnet and has been optimized

--- hacknet.js
+++ hacknet.js
@@ -1,43 +1,30 @@
 /** @param {NS} ns */
 export async function main(ns) {
 	ns.disableLog("sleep")
-	var desnodes = ns.args[0] // use arg for max amount of nodes (23 reccomended max)
+
 
 	while (true) {
+
 
 		var curnodes = ns.hacknet.numNodes()
 
 		await ns.sleep(10)
+		if (curnodes > 0) {
+			for (var i = 0; i < curnodes; i++) {
 
-		while (curnodes < desnodes) {
-			await ns.sleep(10)
-			ns.hacknet.purchaseNode()
-		}
-		if (curnodes >= desnodes) {
-
-			while (true) {
+				if (ns.hacknet.numHashes() > ns.hacknet.hashCapacity() / 10) {
+					ns.hacknet.spendHashes("Sell for Money")
+				}
 
 				await ns.sleep(10)
-
-				for (var i = 0; i < curnodes; i++) {
-					ns.print(i)
-					await ns.sleep(10)
-
-					var node = ns.hacknet.getNodeStats(i)
-
-					if (node.level != 200) {
-						ns.hacknet.upgradeLevel(i)
-					}
-
-					if (node.ram != 64) {
-						ns.hacknet.upgradeRam(i)
-					}
-
-					if (node.cores != 16) {
-						ns.hacknet.upgradeCore(i)
-					}
-				}
+				ns.hacknet.upgradeLevel(i)
+				ns.hacknet.upgradeRam(i)
+				ns.hacknet.upgradeCore(i)
+				ns.hacknet.upgradeCache(i)
+				ns.hacknet.purchaseNode()
 			}
+		} else {
+			ns.hacknet.purchaseNode()
 		}
 	}
 }
